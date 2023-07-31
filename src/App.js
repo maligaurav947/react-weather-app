553c3211ebca52339768eb7adfd352ea44b642f8
Mali Gaurav <maligaurav947@gmail.com>
2023-07-31 21:17:10 +0530

    Add Filed

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { FiMoon, FiSun } from "react-icons/fi";
 import { HiOutlineCloud } from "react-icons/hi";
-import { BsDropletHalf } from "react-icons/bs";
+import { BsDropletHalf ,BsSpeedometer } from "react-icons/bs";
+import {GiPressureCooker} from "react-icons/gi"
+import gif from "./clouds.gif"
 function App() {
   const apiKey = `${process.env.REACT_APP_API_KEY}`;
+
   const [inputCity, setInputCity] = useState("");
   const [data, setData] = useState({});
   const [theme, setTheme] = useState("light");
@@ -17,6 +20,7 @@ function App() {
       setTheme("light");
     }
   };
+
   useEffect(() => {
     document.body.className = theme;
   }, [theme]);
@@ -54,10 +58,10 @@ function App() {
           href="#"
           className="text-2xl flex items-center gap-2 absolute top-2 left-2 font-semibold"
         >
-          <HiOutlineCloud /> react-weather-app
+          <img src={gif} className="w-[50px]"/> react-weather-app
         </a>
         <div className="p-5 lg:p-10">
-          <div className="h-72 flex flex-col items-center justify-center ">
+          <div className="h-56 flex flex-col items-center justify-center ">
             <div
               style={{
                 position: "absolute",
@@ -92,35 +96,47 @@ function App() {
             </div>
           </div>
           {Object.keys(data).length > 0 && (
-            <div className=" text-center mt-5" role="status">
-              <div className="grid items-center">
-                <div className="grid items-center justify-center gap-2">
-                  <h5 className="text-5xl">
-                    {data?.name},<span> {data?.sys?.country}</span>
+            <div className="text-center" role="status">
+              <div className="grid ">
+                <div className="flex flex-col items-center justify-center">
+                  <h5 className="text-5xl flex items-center gap-5">
+                  <img
+                    className="object-contain"
+                    src={`http://openweathermap.org/img/wn/${data?.weather[0]?.icon}@2x.png`}
+                  /><span>
+                  {data?.name},<span> {data?.sys?.country}</span></span>
                   </h5>
-
                   <h2 className="text-2xl capitalize">
                     {data?.weather[0]?.description}
                   </h2>
                 </div>
-
-                <div className="flex items-center justify-evenly border-b-2">
-                  <h6 className="text-6xl gap-2 mb-5 lg:grid">
+                <h6 className="text-6xl gap-2 mb-5 lg:grid">
                     {~~(data?.main?.temp - 273.15).toFixed(2)}°C
-                    <span className="block text-lg text-left">
+                    <span className="text-lg">
                       Feel Like {~~(data?.main?.temp - 273.15).toFixed(2) - 1}°C
                     </span>
                   </h6>
-                  <div>
-                    <h3 className="grid lg:md:xl:flex items-center gap-2">
+                <div className="grid grid-cols-2 gap-5 md:xl:2xl:grid-cols-3 place-content-center">
+                  <div className="border-2 rounded-lg p-2">
+                    <h3 className="flex text-center items-center justify-center gap-2 rounded-lg">
                       <BsDropletHalf size={30} /> {data?.main?.humidity}%
                       humidity
                     </h3>
                   </div>
-                  <img
-                    className="object-contain mb-5"
-                    src={`http://openweathermap.org/img/wn/${data?.weather[0]?.icon}@2x.png`}
-                  />
+                  <div className="border-2 rounded-lg p-2">
+                   <h3 className="flex text-center items-center justify-center gap-2">
+                      <BsSpeedometer size={30}/>
+                    {data?.wind?.speed}
+                    <span>Speed</span>                    
+                    </h3>
+                  </div>
+                  <div className="border-2 rounded-lg p-2">
+                  <h3 className="flex text-center items-center justify-center gap-2">
+                     <GiPressureCooker size={30}/>  
+                    {data?.main?.pressure}  
+                    <span>Pressure</span>                    
+                  </h3>
+                  </div>
                 </div>
               </div>
             </div>
